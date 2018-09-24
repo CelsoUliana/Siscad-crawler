d2697f97935bdf721df07180382df6ea14b7e70f
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2018-09-24 15:41:50 -0300

    Arigato Jurio-kun

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Siscad-crawler
 Node.js Siscad(https://siscad.ufms.br/) crawler
+
+## Usage
+
+Set your siscad login and password in function.
+Then:
+    $ node crawler.js
+or
+    $ nodemon crawler.js
+
+## Como usar
+
+Preencher o form com login e password.
+Em seguida:
+    $ node crawler.js
+ou
+    $ nodemon crawler.js
+
---
+++ nodeCrawler.js
@@ -0,0 +1,147 @@
+// Conforme recomendado pelo Sr.Samurai Jurio Yoshikazu mudanças na organização e funções async
+// Celso Antonio -- September 2018
+// Saudades Julinho
+
+const rp        = require('request-promise')
+const request   = require('request')
+const fs        = require('fs')
+const cheerio   = require('cheerio')
+const Promise   = require('bluebird')
+
+const urlBase               = 'https://siscad.ufms.br/'
+const urlLogin              = 'https://siscad.ufms.br/titan.php'
+const urlNotasFrequencia    = 'https://siscad.ufms.br/titan.php?toSection=14'
+const htmlRes               = 'Notas.html'
+const htmlTable             = 'Tabelas.html'
+
+var urlMateria;
+var j = request.jar()
+
+const logarSiscad = async (login, password) => {
+
+    try{
+
+        // Cria o html final
+        fs.writeFileSync(htmlRes, '')
+        fs.writeFileSync(htmlTable, '')
+
+        let response = await rp({
+            jar: j,
+            uri: urlBase,
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        });
+
+        console.log('Acessou a página principal');
+
+        response = await rp({
+            jar: j,
+            uri: urlLogin,
+            method: 'POST',
+            form: {login, password},
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        })
+
+        console.log('Logou no siscad')
+
+        response = await rp({
+            jar: j,
+            uri: urlNotasFrequencia,
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        })
+
+        console.log('Entrou na Notas/Frequencia')
+
+        let arr = await returnCheerio(response.body);
+
+        let ps = await returnPromises(arr);
+
+        Promise.all(ps).each(async pAtual => {
+
+            console.log('Entrou na materia  ' + pAtual.name);
+
+            response = await rp(pAtual);
+
+            appendTableHtml(response.body, htmlTable);
+            appendFullHtml(response.body, htmlRes);
+        })
+    }
+
+    catch(err) {
+        throw err;
+    }
+};
+
+async function returnPromises(objMaterias){
+
+    var pa = []
+
+    for(var  materia in objMaterias){
+
+        var obj = {
+            jar: j, 
+            name: materia,
+            encoding : 'latin1',  
+            followAllRedirects: true, 
+            resolveWithFullResponse : true, 
+            url : urlBase + objMaterias[materia], 
+        };
+
+        pa.push(obj);
+    }
+
+    return await pa;
+};
+
+async function returnCheerio(responseBody){
+
+    var $ = cheerio.load(responseBody);
+    
+    var objMaterias = {};
+
+    $('div[class="groupHeader"]').first().nextUntil('div[class="groupHeader"]').find('tr').each(function(i, elem){
+        materia = $(this).children().children().text()
+        urlMateria = $(this).children().children().attr('href')
+        materia = materia.replace(/[^a-zA-Z]/g,'')
+
+        if(materia != '')
+            objMaterias[materia] = urlMateria;
+    });
+
+    return await objMaterias;
+};
+
+async function appendFullHtml(responseBody, html){
+
+    fs.appendFile(html, responseBody, 'latin1', async (err) => {
+        if (err) 
+            throw err;
+        console.log('Salvou!');
+    });
+};
+
+async function appendTableHtml(responseBody, html){
+
+    var tableNotas;
+
+    var $ = cheerio.load(responseBody);
+
+    var nomeMateria = $('span[class="infoField"]').text();
+                    
+    $('div[class="infoGroup"]').each(function(i, elem) {
+        if(i === 2){
+            tableNotas = $(this).html()
+        }
+    })
+
+    fs.appendFile(html, `<br>${nomeMateria}<br><br>${tableNotas}<br><br>` ,'latin1',  async (err) => {
+        if (err) 
+            throw err;
+        console.log('Salvou!');
+    });
+}
+
+// username and password goes here
+logarSiscad('SeuLogin', 'SuaSenha')
