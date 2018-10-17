cd0bc90d7e1cd17b3fd5b1dba0d1a9ce4e13d165
Celso Antonio Uliana Junior <32890738+CelsoUliana@users.noreply.github.com>
2018-10-17 18:01:39 -0300

    work 4 now

--- siscad.js
+++ siscad.js
@@ -2,146 +2,147 @@
 // Celso Antonio -- September 2018
 // Saudades Julinho
 
-const rp = require("request-promise");
-const request = require("request");
-const fs = require("fs");
-const cheerio = require("cheerio");
+const rp        = require('request-promise')
+const request   = require('request')
+const fs        = require('fs')
+const cheerio   = require('cheerio')
 
-const urlBase = "https://siscad.ufms.br/";
-const urlLogin = "https://siscad.ufms.br/titan.php";
-const urlNotasFrequencia = "https://siscad.ufms.br/titan.php?toSection=14";
-const htmlRes = "Notas.html";
-const htmlTable = "Tabelas.html";
+const urlBase               = 'https://siscad.ufms.br/'
+const urlLogin              = 'https://siscad.ufms.br/titan.php'
+const urlNotasFrequencia    = 'https://siscad.ufms.br/titan.php?toSection=14'
+const htmlRes               = 'Notas.html'
+const htmlTable             = 'Tabelas.html'
 
-var j = request.jar();
+var j = request.jar()
 
 const logarSiscad = async (login, password) => {
-  try {
-    // Cria o html final
-    fs.writeFileSync(htmlRes, "");
-    fs.writeFileSync(htmlTable, "");
 
-    let response = await rp({
-      jar: j,
-      uri: urlBase,
-      followAllRedirects: true,
-      resolveWithFullResponse: true
-    });
+    try{
 
-    console.log("Acessou a página principal");
+        // Cria o html final
+        fs.writeFileSync(htmlRes, '')
+        fs.writeFileSync(htmlTable, '')
 
-    response = await rp({
-      jar: j,
-      uri: urlLogin,
-      method: "POST",
-      form: { login, password },
-      followAllRedirects: true,
-      resolveWithFullResponse: true
-    });
+        let response = await rp({
+            jar: j,
+            uri: urlBase,
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        })
 
-    console.log("Logou no siscad");
+        console.log('Acessou a página principal')
 
-    response = await rp({
-      jar: j,
-      uri: urlNotasFrequencia,
-      followAllRedirects: true,
-      resolveWithFullResponse: true
-    });
+        response = await rp({
+            jar: j,
+            uri: urlLogin,
+            method: 'POST',
+            form: {login, password},
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        })
 
-    console.log("Entrou na Notas/Frequencia");
+        console.log('Logou no siscad')
 
-    const arr = await getUrlMaterias(response.body);
+        response = await rp({
+            jar: j,
+            uri: urlNotasFrequencia,
+            followAllRedirects: true,
+            resolveWithFullResponse: true,
+        })
 
-    await arr.forEach(async materia => {
-      console.log(`Entrou na materia ${materia.nome}`);
+        console.log('Entrou na Notas/Frequencia')
 
-      const options = {
-        jar: j,
-        encoding: "latin1",
-        followAllRedirects: true,
-        resolveWithFullResponse: true,
-        url: `${urlBase}${materia.url}`
-      };
+        const arr = await getUrlMaterias(response.body)
 
-      response = await rp(options);
+        await arr.forEach(async (materia) => {
 
-      appendTableHtml(response.body, htmlTable);
-      appendFullHtml(response.body, htmlRes);
-    });
-  } catch (err) {
-    throw err;
-  }
-};
+            console.log(`Entrou na materia ${materia.nome}`)
 
-async function appendFullHtml(responseBody, html) {
-  fs.appendFile(html, responseBody, "latin1", async err => {
-    if (err) throw err;
-    console.log("Salvou!");
-  });
+            const options = {
+                jar: j,
+                encoding: 'latin1',
+                followAllRedirects: true,
+                resolveWithFullResponse: true,
+                url: `${urlBase}${materia.url}`,
+            }
+
+            response = await rp(options)
+
+            appendTableHtml(response.body, htmlTable)
+            appendFullHtml(response.body, htmlRes)
+        })
+    } 
+
+    catch(err) {
+        throw err
+    }
 }
 
-async function appendTableHtml(responseBody, html) {
-  var tableNotas;
+async function appendFullHtml(responseBody, html){
 
-  var $ = cheerio.load(responseBody);
+    fs.appendFile(html, responseBody, 'latin1', async (err) => {
+        if (err) 
+            throw err
+        console.log('Salvou!')
+    })
+}
 
-  var nomeMateria = $('span[class="infoField"]').text();
+async function appendTableHtml(responseBody, html){
 
-  $('div[class="infoGroup"]').each(function(i) {
-    if (i === 2) {
-      tableNotas = $(this).html();
-    }
-  });
+    var tableNotas
 
-  fs.appendFile(
-    html,
-    `<br>${nomeMateria}<br><br>${tableNotas}<br><br>`,
-    "latin1",
-    async err => {
-      if (err) throw err;
-      console.log("Salvou!");
-    }
-  );
+    var $ = cheerio.load(responseBody)
+
+    var nomeMateria = $('span[class="infoField"]').text()
+                    
+    $('div[class="infoGroup"]').each(function(i) {
+        if(i === 2){
+            tableNotas = $(this).html()
+        }
+    })
+
+    fs.appendFile(html, `<br>${nomeMateria}<br><br>${tableNotas}<br><br>` ,'latin1',  async (err) => {
+        if (err) 
+            throw err
+        console.log('Salvou!')
+    })
 }
 
 async function getUrlMaterias(responseBody) {
-  const $ = cheerio.load(responseBody);
-  let urlMateria;
-  const arr = [];
 
-  $('div[class="groupHeader"]')
-    .first()
-    .nextUntil('div[class="groupHeader"]')
-    .find("tr")
-    .each((i, elem) => {
-      const objMaterias = {};
-      let materia = $(elem)
-        .children()
-        .children()
-        .text();
-      materia = materia.replace(/[^a-zA-Z ]+/g, "");
-      urlMateria = $(elem)
-        .children()
-        .children()
-        .attr("href");
-
-      if (materia) {
-        objMaterias.nome = materia;
-        objMaterias.url = urlMateria;
-        arr.push(objMaterias);
-      }
-    });
-
-  return await arr;
+    const $ = cheerio.load(responseBody)
+    let urlMateria
+    const arr = []
+  
+    $('div[class="groupHeader"]').first().nextUntil('div[class="groupHeader"]').find('tr')
+        .each((i, elem) => {
+            const objMaterias = {}
+            let materia = $(elem).children().children().text()
+            materia = materia.replace(/[^a-zA-Z ]+/g, '')
+            urlMateria = $(elem).children().children().attr('href')
+  
+            if (materia) {
+                objMaterias.nome = materia
+                objMaterias.url = urlMateria
+                arr.push(objMaterias)
+            }
+        })
+  
+    return await arr
 }
 
-const args = process.argv.slice(2);
+//username and password goes here
+const args = process.argv.slice(2)
+
 if (args.length < 2) {
-  console.log("please provide login and password");
-  process.exit(1);
+  	console.log("please provide RGA and password / Por favor informe RGA e senha")
+  	process.exit(1)
 }
 
-const login = args[0];
-const password = args[1];
+const login 	= args[0]
+const password 	= args[1]
 
-logarSiscad(login, password);
+logarSiscad(login, password)
+
+
+
